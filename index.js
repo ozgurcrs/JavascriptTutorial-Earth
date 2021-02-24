0aad717a856cd658541e9e28b3e4b56d53ca6678
Özgür Çiriş <ozgurcrs@outlook.com>
2021-02-24 19:16:52 +0300

    Initialized Earth Simulation

---
+++ index.js
@@ -0,0 +1,20 @@
+addEventListener("DOMContentLoaded",() => {
+   const earht = document.querySelector(".earth");
+   const MoveEarht = document.querySelector(".moveEarth");
+   const stars = document.querySelector(".stars");
+   let ImageSpeed = 1;
+   setInterval(() => {
+    MoveEarht.style.right = ImageSpeed+'px';
+    earht.style.transform = "rotate("+ImageSpeed/50+"deg)"
+    stars.style.transform =  "rotate("+ImageSpeed/10+"deg)"
+    
+    if(ImageSpeed % 500 == 0){
+        const createImage = document.createElement("img");
+        createImage.src='./earth.png';
+        MoveEarht.appendChild(createImage);
+    }
+  
+    ImageSpeed++;
+
+   },70)
+})
